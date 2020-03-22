35e941fc095be2ea2f0ea729749a1fc3254b6fb0
GodHybrid <6216442+GodHybrid@users.noreply.github.com>
2020-03-22 23:44:46 +0300

    Add files via upload

--- src/test/java/SberIpoteka_Test.java
+++ src/test/java/SberIpoteka_Test.java
@@ -1,18 +1,9 @@
-import Conditions.*;
 import Pages.*;
 
 import Pages.StartPage;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.concurrent.TimeUnit;
 
 public class SberIpoteka_Test
 {
